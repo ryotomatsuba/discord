3a183f0d50baebd495b8aa1ffbb45f013fd3c010
ryoto <metbyroot@gmail.com>
2021-04-15 20:22:07 +0900

    logのファイル指定

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup /home/lab/ryoto/discord/venv/bin/python /home/lab/ryoto/discord/VCNotification.py &
+nohup /home/lab/ryoto/discord/venv/bin/python /home/lab/ryoto/discord/VCNotification.py > out.log 2> error.log &
